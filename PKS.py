7998f614f9cf165375a8d2d40ace15d90d14e423
jfridrich <145677896+xfridrichj@users.noreply.github.com>
2024-10-18 22:49:34 +0200

    prvotna uprava C a S

--- PKS.py
+++ PKS.py
@@ -6,61 +6,70 @@ SERVER_PORT = 50601
 
 class Client:
     def __init__(self, ip, port, server_ip, server_port) ->None:
-        self.sock = socket.socket(socket.AF_INET,
-        socket.SOCK_DGRAM) # UDP socket creation
-        self.server_ip = server_ip
-        self.server_port = server_port
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM); # UDP socket creation
+        self.server_ip = server_ip;
+        self.server_port = server_port;
 
     def receive(self):
-        data = None
-        data, self.server = self.sock.recvfrom(1024) # buffer   size is 1024 bytes
-        return data #1
+        data = None;
+        data, self.server = self.sock.recvfrom(1024); # buffer   size is 1024 bytes
+        return data; #1
 
     def send_message(self, message):
-        self.sock.sendto(bytes(message,encoding="utf8"),(self.server_ip,self.server_port))
+        self.sock.sendto(bytes(message,encoding="utf-8"),(self.server_ip,self.server_port));
 
     def quit(self):
-        self.sock.close() # correctly closing socket
-        print("Client closed..")
+        self.sock.close(); # correctly closing socket
+        print("Client closed..");
 
 class Server:
     def __init__(self, ip, port) -> None:
-        self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP socket creation
-        self.sock.bind((ip, port)) #needs to be tuple (string,int)
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM); # UDP socket creation
+        self.sock.bind((ip, port)); #needs to be tuple (string,int)
 
     def receive(self):
-        data = None
+        data = None;
         while data == None:
-            data, self.client= self.sock.recvfrom(1024) #buffer size is 1024 bytes
-        print("Received message: %s" % data)
+            data, self.client= self.sock.recvfrom(1024); #buffer size is 1024 bytes
+        print("Received message: %s" % data);
         #return data # 1
         return str(data,encoding="utf-8")
 
     def send_response(self):
-        self.sock.sendto(b"Message received... closing connection",self.client)
+        self.sock.sendto(b"Message received... closing connection",self.client);
 
     def quit(self):
-        self.sock.close() # correctly closing socket
-        print("Server closed..")
+        self.sock.close(); # correctly closing socket
+        print("Server closed..");
+
+def run_client():
+    client=Client(CLIENT_IP, CLIENT_PORT, SERVER_IP, SERVER_PORT);
+    message=input("Input your message:");
+    client.send_message(message);
+    response=client.receive();
+    if response:
+        print(response.decode());
+    else:
+        print("Message not received!");
+    client.quit();
+
+def run_server():
+    server=Server(SERVER_IP, SERVER_PORT);
+    data=server.receive();
+    if data:
+        server.send_response();
+    else:
+        print("Message not received!");
+    server.quit();
+
+def main():
+    main_choice=input("Server/Client? (c/s):");
+    if main_choice=='c':
+        run_client();   
+    elif main_choice=='s':
+        run_server();
+    else:
+        print("Error id");
 
 if __name__=="__main__":
-    server = Server(SERVER_IP, SERVER_PORT)
-    data = "empty"
-    data = server.receive() # 1
-    if data != None: # 1
-        server.send_response() # 1
-    else: # 1
-        print("Message has not been received") #1
-    server.quit()
-
-if __name__=="__main__":
-    client = Client(CLIENT_IP, CLIENT_PORT, SERVER_IP,SERVER_PORT)
-    data = "empty"
-    print("Input your message: ") #1
-    client.send_message(input()) # 1
-    data = client.receive() # 1
-    if data != None: # 1
-        print(data.decode()) # 1
-    else: # 1
-        print("Message has not been received") #1
-    client.quit() 
+    main();
