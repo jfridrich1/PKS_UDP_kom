d2b2eb6df4d13ee5b0253edbe453d9556c7bcc9b
jfridrich <jakub.fridrich1@gmail.com>
2024-10-18 23:36:31 +0200

    back to start

--- PKS.py
+++ PKS.py
@@ -1,99 +1,75 @@
 import socket
-import threading
-
-CLIENT_IP = "192.168.1.106"  # Client host IP
-CLIENT_PORT = 50602  # Client port for receiving communication
-SERVER_IP = "192.168.1.108"  # Server host IP
-SERVER_PORT = 50601  # Server port for receiving communication
-
+CLIENT_IP = "192.168.1.106" # client host ip A.B.C.D
+CLIENT_PORT = 50602 # client port for recieving communication
+SERVER_IP = "192.168.1.108" # Server host ip (public IP) A.B.C.D
+SERVER_PORT = 50601
 
 class Client:
-    def __init__(self, ip, port, server_ip, server_port):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP socket creation
-        self.sock.bind((ip, port))  # Bind the client to its IP and port
-        self.server_ip = server_ip
-        self.server_port = server_port
+    def __init__(self, ip, port, server_ip, server_port) ->None:
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM); # UDP socket creation
+        self.server_ip = server_ip;
+        self.server_port = server_port;
+
+    def receive(self):
+        data = None;
+        data, self.server = self.sock.recvfrom(1024); # buffer   size is 1024 bytes
+        return data; #1
 
     def send_message(self, message):
-        """Send a message to the server."""
-        self.sock.sendto(message.encode("utf-8"), (self.server_ip, self.server_port))
-
-    def receive(self):
-        """Receive messages from the server."""
-        while True:
-            data, _ = self.sock.recvfrom(1024)  # Buffer size is 1024 bytes
-            print("Server says: %s" % data.decode("utf-8"))
+        self.sock.sendto(bytes(message,encoding="utf-8"),(self.server_ip,self.server_port));
 
     def quit(self):
-        """Close the client socket."""
-        self.sock.close()
-        print("Client closed..")
-
+        self.sock.close(); # correctly closing socket
+        print("Client closed..");
 
 class Server:
-    def __init__(self, ip, port):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP socket creation
-        self.sock.bind((ip, port))  # Bind the server to its IP and port
+    def __init__(self, ip, port) -> None:
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM); # UDP socket creation
+        self.sock.bind((ip, port)); #needs to be tuple (string,int)
 
     def receive(self):
-        """Receive messages from the client."""
-        while True:
-            data, self.client = self.sock.recvfrom(1024)  # Buffer size is 1024 bytes
-            print("Client says: %s" % data.decode("utf-8"))
+        data = None;
+        while data == None:
+            data, self.client= self.sock.recvfrom(1024); #buffer size is 1024 bytes
+        print("Received message: %s" % data);
+        #return data # 1
+        return str(data,encoding="utf-8")
 
-    def send_response(self, message):
-        """Send a message back to the client."""
-        self.sock.sendto(message.encode("utf-8"), self.client)
+    def send_response(self):
+        self.sock.sendto(b"Message received... closing connection",self.client);
 
     def quit(self):
-        """Close the server socket."""
-        self.sock.close()
-        print("Server closed..")
-
+        self.sock.close(); # correctly closing socket
+        print("Server closed..");
 
 def run_client():
-    client = Client(CLIENT_IP, CLIENT_PORT, SERVER_IP, SERVER_PORT)
-
-    # Start a thread to receive messages from the server
-    receive_thread = threading.Thread(target=client.receive)
-    receive_thread.daemon = True  # This ensures the thread will exit when the main program exits
-    receive_thread.start()
-
-    # Sending messages in the main thread
-    while True:
-        message = input("You: ")
-        if message.lower() == "exit":
-            client.quit()
-            break
-        client.send_message(message)
-
+    client=Client(CLIENT_IP, CLIENT_PORT, SERVER_IP, SERVER_PORT);
+    message=input("Input your message:");
+    client.send_message(message);
+    response=client.receive();
+    if response:
+        print(response.decode());
+    else:
+        print("Message not received!");
+    client.quit();
 
 def run_server():
-    server = Server(SERVER_IP, SERVER_PORT)
-
-    # Start a thread to receive messages from the client
-    receive_thread = threading.Thread(target=server.receive)
-    receive_thread.daemon = True  # This ensures the thread will exit when the main program exits
-    receive_thread.start()
-
-    # Sending responses in the main thread
-    while True:
-        message = input("You: ")
-        if message.lower() == "exit":
-            server.quit()
-            break
-        server.send_response(message)
-
+    server=Server(SERVER_IP, SERVER_PORT);
+    data=server.receive();
+    if data:
+        server.send_response();
+    else:
+        print("Message not received!");
+    server.quit();
 
 def main():
-    main_choice = input("Server/Client? (s/c): ").lower()
-    if main_choice == 'c':
-        run_client()
-    elif main_choice == 's':
-        run_server()
+    main_choice=input("Server/Client? (c/s):");
+    if main_choice=='c':
+        run_client();   
+    elif main_choice=='s':
+        run_server();
     else:
-        print("Invalid choice.")
+        print("Error id");
 
-
-if __name__ == "__main__":
-    main()
+if __name__=="__main__":
+    main();
