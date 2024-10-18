a024173e93e343697a3da9aa87a91c5911152fc3
jfridrich <jakub.fridrich1@gmail.com>
2024-10-19 00:01:09 +0200

    handshake2.5

--- PKS.py
+++ PKS.py
@@ -15,6 +15,22 @@ class Client:
         data, self.server = self.sock.recvfrom(1024); # buffer   size is 1024 bytes
         return data; #1
 
+    def three_way_handshake(self):
+        # Step 1: Send SYN to the server
+        print("Sending SYN to the server...")
+        self.send_message("SYN")
+        
+        # Step 2: Receive SYN-ACK from the server
+        response = self.receive()
+        if response == "SYN-ACK":
+            print("Received SYN-ACK from the server...")
+            
+            # Step 3: Send ACK to the server
+            print("Sending ACK to the server...")
+            self.send_message("ACK")
+            return True
+        return False
+
     def send_message(self, message):
         self.sock.sendto(bytes(message,encoding="utf-8"),(self.server_ip,self.server_port));
 
@@ -27,13 +43,6 @@ class Server:
         self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM); # UDP socket creation
         self.sock.bind((ip, port)); #needs to be tuple (string,int)
 
-<<<<<<< Updated upstream
-    def receive_message(self):
-        """Receive messages from the client (or peer)."""
-        while True:
-            data, self.client = self.sock.recvfrom(1024)  # Buffer size is 1024 bytes
-            print("Received from client:", data.decode("utf-8"))
-=======
     def receive(self):
         data = None;
         while data == None:
@@ -59,7 +68,6 @@ class Server:
                 print("Received ACK from client... Connection established!")
                 return True
         return False
->>>>>>> Stashed changes
 
     #def send_response(self):
     #    self.sock.sendto(b"Message received... closing connection",self.client);
@@ -71,40 +79,6 @@ class Server:
         print("Server closed..");
 
 def run_client():
-<<<<<<< Updated upstream
-    client = Client(CLIENT_IP, CLIENT_PORT, SERVER_IP, SERVER_PORT)
-
-    # Start a thread for receiving messages from the server (peer)
-    receive_thread = threading.Thread(target=client.receive_message)
-    receive_thread.daemon = True  # Daemon thread exits when the main program exits
-    receive_thread.start()
-
-    # Send messages in the main thread
-    while True:
-        message = input("Input your message (type 'exit' to quit): ")
-        if message.lower() == "exit":
-            client.quit()
-            break
-        client.send_message(message)
-
-
-def run_server():
-    server = Server(SERVER_IP, SERVER_PORT)
-
-    # Start a thread for receiving messages from the client (peer)
-    receive_thread = threading.Thread(target=server.receive_message)
-    receive_thread.daemon = True  # Daemon thread exits when the main program exits
-    receive_thread.start()
-
-    # Send responses in the main thread
-    while True:
-        message = input("Input response (type 'exit' to quit): ")
-        if message.lower() == "exit":
-            server.quit()
-            break
-        server.send_response(message)
-
-=======
     client=Client(CLIENT_IP, CLIENT_PORT, SERVER_IP, SERVER_PORT);
     """message=input("Input your message:");
     client.send_message(message);
@@ -141,7 +115,6 @@ def run_server():
     else:
         print("Handshake failed.")
     server.quit();
->>>>>>> Stashed changes
 
 def main():
     main_choice=input("Server/Client? (c/s):");
