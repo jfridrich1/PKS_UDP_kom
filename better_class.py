38ae764dea21f11bae5e491c4009e496f9499271
jfridrich <jakub.fridrich1@gmail.com>
2024-10-20 00:54:47 +0200

    cleaning trosku

--- better_class.py
+++ better_class.py
@@ -2,152 +2,190 @@ import socket
 import threading
 import struct
 
+CLIENT_IP = "127.0.0.1"
+CLIENT_PORT = 50602
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 50601
+
 class Header:
     def __init__(self, flags, payload_size, total_frag, frag_offset, checksum, payload) -> None:
-        #8B header
-        self.flags = flags
-        self.payload_size = payload_size
-        self.total_frag = total_frag
-        self.frag_offset = frag_offset
-        self.checksum = checksum
-        self.payload = payload
+        self.flags=flags
+        self.payload_size=payload_size
+        self.total_frag=total_frag
+        self.frag_offset=frag_offset
+        self.checksum=checksum
+        self.payload=payload
 
+    #toto bude metoda ktora vola vsetky ostatne funkcne metody (fragment, ...)
     def build_packet(self):
         if isinstance(self.payload, str):
-            self.payload = self.payload.encode('utf-8')
-        head = struct.pack('!B H B H H', self.flags, self.payload_size, self.total_frag, self.frag_offset, self.checksum)
-        return head + self.payload
+            self.payload=self.payload.encode('utf-8')
+        head=struct.pack('!B H B H H', self.flags, self.payload_size, self.total_frag, self.frag_offset, self.checksum)
+        return head+self.payload
 
-    @staticmethod
+    @staticmethod #nepotrebuje Header instance
+    #spracuj packet
     def parse_packet(packet):
-        head = packet[:8]
-        flags, payload_size, total_frag, frag_offset, checksum = struct.unpack('!B H B H H', head)
-        payload = packet[8:8+payload_size]
-        return {'flags': flags, 'payload_size': payload_size, 'total_frag': total_frag, 'frag_offset': frag_offset, 'checksum': checksum, 'payload': payload}
+        head=packet[:8]   #8 je velkost headeru
+        flags, payload_size, total_frag, frag_offset, checksum=struct.unpack('!B H B H H', head)#1B,2B,1B,2B,2B
+        payload=packet[8:8+payload_size] #data/sprava
+        return {'flags':flags, 'payload_size':payload_size, 'total_frag':total_frag, 'frag_offset':frag_offset, 'checksum':checksum, 'payload':payload}
 
-# Common base class for both Client and Server
+#zaklad pre Klienta a Server
 class Peer:
     def __init__(self, ip, port) -> None:
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP socket
-        self.sock.bind((ip, port))
-        self.client = None
-        self.running_th = True
+        self.sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((ip,port))   #bind k Peer-ovi (client/server)
+        self.running_th=True        #threading flag
+        self.peer_address=None      #priprava na ulozenie adresy (klienta ak je server, servera ak je klient)
 
-    def send_message(self, message, target=None):
+    #odosielanie sprav - pre SYN, SYN ACK, ACK
+    def send_message(self, message, receiver=None):
         if isinstance(message, str):
-            message = message.encode('utf-8')
-        if target is None and self.client:
-            target = self.client
-        if target:
-            self.sock.sendto(message, target)
+            message=message.encode('utf-8')
+        if receiver is None: #errir handle
+            receiver=self.peer_address
+        if receiver:
+            self.sock.sendto(message, receiver)
 
-    def receive(self):
-        while self.running_th:
-            try:
-                data, self.client = self.sock.recvfrom(1024)
-                packet = Header.parse_packet(data)
-                print(f"\nReceived packet: {packet}")
-            except Exception as e:
-                print(f"Error in receiving: {e}")
-                break
+    #metoda prijatia packetu
+    def receive_packet(self):
+        while self.running_th:  #threadovanie 
+            data, self.peer_address=self.sock.recvfrom(1024) #zmenit velkost neskor
+            #rozober packet
+            packet=Header.parse_packet(data)
 
-    def send(self):
-        while self.running_th:
-            message = input(f"You ({self.__class__.__name__}): ")
-            flags = 1
-            payload_size = len(message)
-            total_frag = 2
-            frag_offset = 3
-            checksum = 1234
-            packet = Header(flags, payload_size, total_frag, frag_offset, checksum, message)
+            #debug
+            print(f"\nReceived packet: {packet}")
+            print(f"Received message: {packet['payload']}")
+
+    #metoda odoslania packetu
+    def send_packet(self):
+        while self.running_th:  #threadovanie
+            #debug, neskor prec
+            data=input(f"You ({self.__class__.__name__}): ")
+
+            #default hodnoty na debug
+            flags=1
+            payload_size=len(data)
+            total_frag=2
+            frag_offset=3
+            checksum=1234
+
+            #volanie funkcii spracovania poli (fragmetnt, MT, flagy,...) TU
+
+            #skladanie packetu,inc 
+            packet=Header(flags, payload_size, total_frag, frag_offset, checksum, data)
             self.send_message(packet.build_packet())
 
-            if message.lower() == "quit":
+            #pokus o ukoncenie spojenia
+            """print(f"toto je data: {data}")
+            if data.lower() == "quit":
                 self.running_th = False
-                break
+                break"""
 
-    def start(self):
-        recv_thread = threading.Thread(target=self.receive)
-        send_thread = threading.Thread(target=self.send)
+    #metoda na vymienanie sprav
+    def chatting(self):
+        recv_thread=threading.Thread(target=self.receive_packet)
+        send_thread=threading.Thread(target=self.send_packet)
         recv_thread.start()
         send_thread.start()
         send_thread.join()
 
+    #metoda na ukoncenie spojenia - zatial iba jednostranne
     def quit(self):
-        self.running_th = False
-        self.sock.close()
+        self.running_th=False   #koniec threadovania
+        self.sock.close()       #uzavri socket
+
+        #debug, neskor prec
         print(f"{self.__class__.__name__} closed...")
 
-# Client class inheriting from Peer
 class Client(Peer):
     def __init__(self, ip, port, server_ip, server_port) -> None:
         super().__init__(ip, port)
-        self.server_ip = server_ip
-        self.server_port = server_port
+        self.server_ip=server_ip
+        self.server_port=server_port
+        self.peer_address=(server_ip, server_port)  #servers address na posielanie
 
-    def three_way_handshake(self):
+    #klientova verzia 3w shaku
+    def three_way_hs_c(self):
+        #start 3wHS, debug
         print("Sending SYN to the server...")
-        self.send_message("SYN", (self.server_ip, self.server_port))
-        response, _ = self.sock.recvfrom(1024)
-        response = response.decode('utf-8')
-        if response == "SYN-ACK":
+        self.send_message("SYN", (self.server_ip, self.server_port))  #serverova adresa
+
+        #odpoved na SYN
+        response, self.peer_address = self.sock.recvfrom(1024)
+        response=response.decode('utf-8')
+        if response=="SYN-ACK":
+
+            #debug
             print("Received SYN-ACK from the server...")
             print("Sending ACK to the server...")
-            self.send_message("ACK", (self.server_ip, self.server_port))
+            self.send_message("ACK", self.peer_address)  #posli ACK serveru
             return True
         return False
 
-# Server class inheriting from Peer
 class Server(Peer):
     def __init__(self, ip, port) -> None:
         super().__init__(ip, port)
 
-    def three_way_handshake(self):
+    #serverova verzia 3w shaku
+    def three_way_hs_s(self):
+        #vypis po zvoleni servera
         print("Waiting for SYN from client...")
-        message, self.client = self.sock.recvfrom(1024)
-        message = message.decode('utf-8')
-        if message == "SYN":
+
+        #prichod SYN
+        message, self.peer_address=self.sock.recvfrom(1024) #klientova adresa
+        message=message.decode('utf-8')
+        if message=="SYN":
             print("Received SYN from client...")
+
+            #posli odpoved na SYN -> SYN-ACK, debug
             print("Sending SYN-ACK to the client...")
-            self.send_message("SYN-ACK")
-            ack, _ = self.sock.recvfrom(1024)
-            if ack.decode('utf-8') == "ACK":
-                print("Received ACK from client... Connection established!")
+            self.send_message("SYN-ACK", self.peer_address)
+
+            #odpoved na SYN-ACK -> ACK
+            ack, _=self.sock.recvfrom(1024)
+            if ack.decode('utf-8')=="ACK":
+
+                #prijaty ACk, debug
+                print("Received ACK from client, connection complete!")
                 return True
         return False
 
-# Running the client
+#klient main
 def run_client():
-    client = Client(CLIENT_IP, CLIENT_PORT, SERVER_IP, SERVER_PORT)
-    if client.three_way_handshake():
-        print("Handshake successful. Ready to chat...")
-        client.start()
+    client=Client(CLIENT_IP, CLIENT_PORT, SERVER_IP, SERVER_PORT)
+    if client.three_way_hs_c()==True:
+        print("Handshake successful. Chat ready!")
+        client.chatting()
     else:
-        print("Handshake failed.")
+        print("Handshake error.")
     client.quit()
 
-# Running the server
+#server main
 def run_server():
     server = Server(SERVER_IP, SERVER_PORT)
-    if server.three_way_handshake():
-        print("Handshake successful. Ready to chat...")
-        server.start()
+    if server.three_way_hs_s()==True:
+        print("Handshake successful. Chat ready!")
+        server.chatting()
     else:
-        print("Handshake failed.")
+        print("Handshake error.")
     server.quit()
 
 def main():
-    main_choice = input("Server/Client? (c/s): ")
-    if main_choice == 'c':
+    main_choice=input("Server/Client? (c/s): ")
+    if main_choice=='c':
         run_client()
-    elif main_choice == 's':
+    elif main_choice=='s':
         run_server()
     else:
-        print("Invalid choice")
+        print("Invalid command.")
 
-if __name__ == "__main__":
-    CLIENT_IP = "127.0.0.1"
-    CLIENT_PORT = 50602
-    SERVER_IP = "127.0.0.1"
-    SERVER_PORT = 50601
+if __name__=="__main__":
     main()
+
+
+#upravit velkost bufferu - 1024
+#doriesit printy ako You (trieda) - odstranit / fixnut
+#ako seknut komunikaciu
